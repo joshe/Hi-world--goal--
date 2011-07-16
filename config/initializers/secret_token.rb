c945aa37badf98d3f50301a5b9d32755fc16158a
Josh Evnin <jevnin@gmail.com>
2011-07-16 17:12:31 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goalie::Application.config.secret_token = '3998a1cd09709529c339be84ce9f38ea4db0cb5f4c9861cfdca7ac27dd54cdfda395d2deaa88e0a1f6f4cedeef06dc5dd87083c53fce7af27ec0693c83db8d58'
