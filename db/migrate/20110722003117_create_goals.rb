9309991685dedd917bfd9e98c31657d40e8aeb3e
Josh Evnin <jevnin@gmail.com>
2011-07-30 15:55:12 -0500

    fixing migration issue

--- db/migrate/20110722003117_create_goals.rb
+++ db/migrate/20110722003117_create_goals.rb
@@ -1,8 +1,8 @@
 class CreateGoals < ActiveRecord::Migration
   def self.up
     create_table :goals do |t|
-      t.string "name"
-      t.string "type"     # eg. run/walk/exercise
+      t.string "goal_name"
+      t.string "goal_type"     # eg. run/walk/exercise
       t.integer "count", :null => false     # eg. 1/5/30
       t.string "measure", :null => false    # eg. mi/km
       t.integer "time_count", :null => false  # eg. 1/5/30
