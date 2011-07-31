b7842038a07005b5c6b4380b67e30a70139872a1
jevnin <jevnin@gmail.com>
2011-07-31 16:10:04 -0500

    Created add_new field for goal_type

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,17 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function(){
+	
+	
+	
+	$("#goal_goal_type").live("change", function(){
+		var selectedVal = $(this).children("option:selected").val();
+		if(selectedVal == "Add new") {
+			$("#goal_goal_type").after("<input type='text' id='goal_goal_type' name='goal[goal_type]' placeholder='new goal type'/>").remove();
+		}
+	});
+	
+	
+	
+});
